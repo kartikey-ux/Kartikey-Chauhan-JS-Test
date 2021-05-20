556b1704413f3e2429fb4cc27fad8cc656bb7921
kartikey-ux <84465692+kartikey-ux@users.noreply.github.com>
2021-05-21 00:09:22 +0530

    Add files via upload

---
+++ Question1.js
@@ -0,0 +1,7 @@
+JavaScript
+for(i=10; i<=20; i++){
+
+  if(i % 2 == 0){
+    console.log(i);
+  }
+}
---
+++ Question7.js
@@ -0,0 +1,4 @@
+$http.get("http://www.example.com/api/get/1").then(function(response)=>{
+  console.log(response)
+}
+)
